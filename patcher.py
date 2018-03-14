d598cf86e06a369aef2ae9c6cd65786aed69ecc2
Asutosh Palai <asupalai@gmail.com>
2018-03-14 12:10:27 +0530

    Fixed dropping of previlege in main func

--- patcher.py
+++ patcher.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python2
 """
-Script to patch adbd binary by ro.debuggable check
-
-We could have modified the binary through r2 but there is a bug due to which
-r2 makes unnessary writes. Refer to the following GitHub issue for details:
-    https://github.com/radare/radare2/issues/9489
-
-As a hack, we are reading about 40 bytes, making there required changes through
-hardcoded asm codes and using string find and replace over the binary.
+Script to patch adbd binary by ro.debuggable check and privilege dropping
 """
 
 import sys
+import json
+import shutil
 import r2pipe
 
 if len(sys.argv) != 3:
@@ -20,17 +15,16 @@ if len(sys.argv) != 3:
 input = sys.argv[1]
 output = sys.argv[2]
 
-import r2pipe
-r2 = r2pipe.open(input)
-r2.cmd('af')
-orig = r2.cmd('p8 40 @ sym.__android_log_is_debuggable').decode('hex')
-new = '200080d2'.decode('hex') + 'c0035fd6'.decode('hex') + orig[8:]
+shutil.copy2(input, output)
+
+r2 = r2pipe.open(output, ['-w'])
+print(r2.cmd('aa'))
+print(r2.cmd('s sym.__android_log_is_debuggable'))
+print(r2.cmd('"wa mov x0, 1; ret"'))
+nop_address = [json.loads(r2.cmd("axtj sym.minijail_capbset_drop"))[0]['from'],
+                json.loads(r2.cmd("axtj sym.minijail_change_gid"))[0]['from'],
+                json.loads(r2.cmd("axtj sym.minijail_change_uid"))[0]['from']]
+
+for addr in nop_address:
+    r2.cmd('wa nop @ %d' % addr)
 r2.quit()
-
-with open(input, 'r') as f:
-    bin = f.read()
-
-bin = bin.replace(orig, new)
-
-with open(output, 'wb') as f:
-    f.write(bin)
